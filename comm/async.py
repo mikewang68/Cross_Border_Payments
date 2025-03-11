c6b14c2440225964f71c69495a39e5f7ce97c151
842457745 <842457745@qq.com>
2025-03-11 13:28:24 +0800

    同步修改

--- comm/async.py
+++ comm/async.py
@@ -7,10 +7,23 @@ from db_api import update_database
 from gsalay_api import GSalaryAPI
 
 
-
 #-------------------------- 伽利略任务Start------------------------------------------------------
 
 gsalary = GSalaryAPI()
+time = 60
+
+
+
+async def async_time (version):
+    # ------------------- 获取执行周期
+    async_ctrl = query_database('async_ctrl', 'version', 'J1')
+    record = async_ctrl[0]
+    time = record.get('async_time')
+    print('获取时间：',time)
+    return time
+
+
+
 # 插入交易明细
 async def card_transactions(version):
     data = gsalary.get_card_transactions(version)
@@ -25,54 +38,33 @@ async def balance_history(version):
 #-------------------------- 伽利略任务End--------------------------------------------------------
 
 
+# 模拟获取用户信息
+async def get_user_info():
+
+    return
 
 
+# 主任务函数，异步执行所有信息获取
+async def fetch_info():
+    tasks = [
+        card_transactions('J1'),
+        balance_history('J1'),
+        get_user_info(),
+        async_time('J1'),
+    ]
+
+    # 使用 asyncio.gather 并行执行所有任务
+    await asyncio.gather(*tasks)
 
 
+    await asyncio.sleep(int(time))
 
 
+# 运行异步程序
+async def main():
+    # 启动定时任务
+    await fetch_info()
 
 
-
-
-
-
-
-
-
-# --------------------------------任务定时启动Start-----------------------------------------------------
-async def periodic_task():
-    while True:
-
-
-#--------------------伽利略1
-        # 插入交易明细
-        await card_transactions('J1')
-
-        # 插入余额明细
-        await balance_history('J1')
-
-
-#--------------------伽利略2
-        # 插入交易明细
-        await card_transactions('J2')
-
-        # 插入余额明细
-        await balance_history('J2')
-
-
-#------------------- 获取执行周期
-        async_ctrl = query_database('async_ctrl','version','J1')
-        record = async_ctrl[0]
-        sleep_time = record.get('async_time')
-        await asyncio.sleep(int(sleep_time))
-
-# --------------------------------任务定时启动End-------------------------------------------------------
-
-
-
-
-
-
-if __name__ == "__main__":
-    asyncio.run(periodic_task())
+# 启动程序
+asyncio.run(main())
